54158c7ed8956ac58f7551768bc8e9386bff50e2
Robin <robin@pernaudat.com>
2023-06-26 08:28:24 +0200

    dev du train

--- components/App.js
+++ components/App.js
@@ -2,23 +2,34 @@ import Head from './Head';
 import LeftCard from './LeftCard';
 import Motivation from './Motivation';
 
-const l_style={
+const l_style = {
   marginLeft: 'auto',
   marginRight: 'auto',
   padding: '10px',
   maxWidth: '1024px',
-  backgroundColor: 'bisque',
+  backgroundColor: 'rgb(1, 93, 129)',
   borderRadius: "25px",
-  
+  color: "rgb(239, 241, 241)",
 
 };
 export default class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { liked: false };
-    }
-    render(){return (<div style={l_style}><div style={{display: 'flex',}}><LeftCard/><Head/></div>
-    <Motivation/></div>);}
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+  render() {
+    return (<div className="container-fluid" style={l_style}>
+      <div class="row">
+        <div class="col">
+          <LeftCard />
+        </div>
+        <div class="col">
+          <Head />
+        </div>
+      </div>
+
+      <Motivation /></div>);
+  }
 }
 
-const domContainer = document.querySelector('html > body');ReactDOM.render(<App/>, domContainer);
+const domContainer = document.querySelector('html > body'); ReactDOM.render(<App />, domContainer);
--- components/LeftCard.js
+++ components/LeftCard.js
@@ -1,35 +1,47 @@
+require('bootstrap');
 
-const style={
-    width: "660px",
-    minHeight: "200px",
-    border: 'solid 1px #100010',
-    color:'#100010',
-    boxShadow: '7px 5px 5px #100010',
-    padding: "5px",
-    margin: "0px",
-    display: 'flex',
-    position: 'relative',
-    
+const style = {
+  width: "32rem",
+  maxWidth: "800px",
+  //minHeight: "200px",
+  border: 'solid 1px #100010',
+  color: "rgb(239, 241, 241)",
+  boxShadow: '7px 5px 5px #100010',
+  //padding: "5px",
+  //margin: "0px",
+  //display: 'flex',
+  //position: 'relative',
+
 };
 
 export default class LeftCard extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { liked: false };
-    }
-    render(){return (<div style={style}><div>
-      <h1 style={{margin: '0px',textDecoration: 'underline',}}>Robin PERNAUDAT</h1>
-      <br/>
-      <ul style={{listStyleType: 'none',}}>
-      <li>06.33.55.28.73</li>
-      <li>robin@pernaudat.com</li>
-      <li>330 allée des blés d'or</li>
-      <li>01310 BUELLAS</li>
-      <br/>
-      <li>25 janvier 1986</li>
-      <li>Permis B</li>
-      </ul>
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+  render() {
+    return (<div className="card xd-3" style={style}>
+      <div className="row g-0">
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title" style={{ textDecoration: 'underline', }}>
+              Robin PERNAUDAT
+            </h5>
+            <p class="card-text">
+              <li>06.33.55.28.73</li>
+              <li>robin@pernaudat.com</li>
+              <li>330 allée des blés d'or</li>
+              <li>01310 BUELLAS</li>
+              <br />
+              <li>25 janvier 1986</li>
+              <li>Permis B</li>
+            </p>
+          </div>
+        </div>
+        <div className="col-md-4" >
+          <img className="img-fluid rounded-start" style={{ height: "225px", }} src="images/portrait vert2_short.JPG" />
+          </div>
       </div>
-      <div style={{  position: "absolute", right: "5px", padding: '0px', margin :'0px',}}><img style={{height: "225px",}} src="images/portrait vert2_short.JPG"/></div>
-      </div>);}
+    </div>);
+  }
 }
